a426ee65c33f8b3d0109fd19f0306e4f4218e7bf
savinsaka <savinsaka@gmail.com>
2021-04-22 15:37:46 +0700

    pertemuan8

--- controller/kegiatan.js
+++ controller/kegiatan.js
@@ -1,4 +1,5 @@
 const kegiatanModel = require('../model/kegiatan')
+const objectid = require('mongoose').Types.ObjectId
 
 exports.create= (data) =>
   new Promise((resolve, reject)=> {
@@ -10,5 +11,66 @@ exports.create= (data) =>
       })).catch(()=> ({
         status : false,
         pesan : 'gagal iput'
+      }))
+  })
+
+  exports.showAllData = () =>
+    new Promise((resolve, reject)=>{
+      kegiatanModel.find()
+      .then (result =>{
+        resolve({
+          status : true,
+          pesan : 'berhasil menampilkan data',
+          data : result
+        })
+      }).catch(()=> reject ({
+        status : false,
+        pesan : 'gagal menampilkan data',
+        data : []
+      }))
+    })
+
+    exports.showbyID = (id) =>
+      new Promise((resolve, reject) => {
+        kegiatanModel.findOne({
+          _id: objectid(id)
+        })
+        .then (result =>{
+          resolve({
+            status : true,
+            pesan : 'berhasil menampilkan data',
+            data : result
+          })
+        })
+        .catch(()=> reject ({
+          status : false,
+          pesan : 'gagal menampilkan data',
+          data : null
+        }))
+      })
+
+exports.update = (id, data)=>
+  new Promise((resolve, reject)=>{
+    kegiatanModel.updateOne({
+      _id: objectid(id)
+    }, data).then (() => resolve({
+        status : true,
+        pesan : 'berhasil update data',
+      })).catch(()=> reject ({
+      status : false,
+      pesan : 'gagal update data',
+    }))
+  })
+
+exports.delete = (id) =>
+  new Promise((resolve, reject)=>{
+    kegiatanModel.deleteOne({
+      _id: objectid(id)
+    }).then (() => resolve({
+      status : true,
+      pesan : 'berhasil delete data',
+      })).catch(()=> reject ({
+      status : false,
+      pesan : 'gagal delete data',
       }))
   })
